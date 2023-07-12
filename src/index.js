ca2dba43e9fe116d82ce06edd4c37d04836b6d70
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-12 15:49:29 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import './style.css';
+
+const components = [
+  {
+    description: 'complete to Do list project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+];
+
+const showComponents = () => {
+    const container = document.querySelector('.to-do-list');
+
+    // Sort components array based on index property
+    components.sort((a, b) => a.index - b.index);
+  
+    components.forEach((component) => {
+      const items = document.createElement('li');
+      items.className = 'list-item';
+      items.innerHTML = `<input class="check-box" type="checkbox">${component.description}`;
+      container.appendChild(items);
+    });
+  };
+
+  window.addEventListener('load', showComponents);
