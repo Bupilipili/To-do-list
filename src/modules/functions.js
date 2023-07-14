4b4c1aab2be12adf7d07a253993b5b6c7e26e110
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-14 20:06:32 +0300

    Create functions.js

---
+++ src/modules/functions.js
@@ -0,0 +1,21 @@
+
+export const addNewComponent = (componentDesc, componentsLength) => {
+    const newComponent = {
+      description: componentDesc,
+      completed: false,
+      index: componentsLength,
+    };
+  
+    return newComponent;
+  };
+  
+  export const removeComponents = (components, index) => {
+    components.splice(index, 1);
+    let i = index;
+    while (i < components.length) {
+      components[i].index = i;
+      i += 1;
+    }
+  };
+
+  
