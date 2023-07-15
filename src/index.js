61595169e0a9f40854f4c1393355395a36af4045
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-15 19:40:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,103 +1,67 @@
 import './style.css';
-
 import { addNewComponent, removeComponents } from './modules/functions.js';
+import updateComponents from './modules/updateComponents';
+import showComponents from './modules/showComponents';
+import clearCompletedTasks from './modules/clearCompletedTasks';
 
-const showComponents = (components) => {
-  const container = document.querySelector('.to-do-list');
-  container.innerHTML = ''; // Clears the container
-
-  // Sort components array based on index property
-  components.sort((a, b) => a.index - b.index);
-
-  components.forEach((component) => {
-    const items = document.createElement('li');
-    items.className = 'list-item';
-    items.dataset.index = component.index - 1;
-    items.innerHTML = `
-    <div class="listboxflex">
-        <input class="check-box" type="checkbox" ${component.completed ? 'checked' : ''}>
-        <input class="description" value="${component.description}" readonly>
-        <a><i class="fa-solid fa-trash delete-icon"></i></a>
-    </div>
-    `;
-    container.appendChild(items);
-  });
-};
-
-let components = JSON.parse(localStorage.getItem('toDoList')) || [];
-const addList = document.querySelector('.input');
-addList.addEventListener('keyup', (event) => {
-  event.preventDefault();
-  if (event.key === 'Enter' && addList.value !== '') {
-    const componentDesc = addList.value;
-    components.push(addNewComponent(componentDesc, components.length));
-    showComponents(components);
-    localStorage.setItem('toDoList', JSON.stringify(components));
-    addList.value = '';
-  }
-});
-
-const container = document.querySelector('.to-do-list');
-container.addEventListener('click', (event) => {
-  const remove = document.querySelectorAll('.delete-icon');
-  remove.forEach((icon, index) => {
-    if (event.target === icon) {
-      removeComponents(components, index);
+  let components = JSON.parse(localStorage.getItem('toDoList')) || [];
+  const addList = document.querySelector('.input');
+  addList.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    if (event.key === 'Enter' && addList.value !== '') {
+      const componentDesc = addList.value;
+      components.push(addNewComponent(componentDesc, components.length));
       showComponents(components);
       localStorage.setItem('toDoList', JSON.stringify(components));
+      addList.value = '';
     }
   });
 
-  // Edit case
-  const tasksInput = document.querySelectorAll('.description');
-  tasksInput.forEach((input) => {
-    input.addEventListener('input', (event) => {
-      const { index } = event.target.closest('.list-item').dataset;
-      if (components[index]) {
-        components[index].description = event.target.value;
+  const container = document.querySelector('.to-do-list');
+  container.addEventListener('click', (event) => {
+    const remove = document.querySelectorAll('.delete-icon');
+    remove.forEach((icon, index) => {
+      if (event.target === icon) {
+        removeComponents(components, index);
+        showComponents(components);
         localStorage.setItem('toDoList', JSON.stringify(components));
       }
     });
-    input.addEventListener('click', () => {
-      if (input.hasAttribute('readonly')) {
-        input.removeAttribute('readonly');
-      } else {
-        input.setAttribute('readonly', 'true');
-      }
+
+    // Edit case
+    const tasksInput = document.querySelectorAll('.description');
+    tasksInput.forEach((input) => {
+      input.addEventListener('input', (event) => {
+        const { index } = event.target.closest('.list-item').dataset;
+        if (components[index]) {
+          components[index].description = event.target.value;
+          localStorage.setItem('toDoList', JSON.stringify(components));
+        }
+      });
+      input.addEventListener('click', () => {
+        if (input.hasAttribute('readonly')) {
+          input.removeAttribute('readonly');
+        } else {
+          input.setAttribute('readonly', 'true');
+        }
+      });
+    });
+
+    // Checkbox status
+    const checkBoxes = document.querySelectorAll('.check-box');
+    checkBoxes.forEach((checkBox, index) => {
+      checkBox.addEventListener('change', () => {
+        updateComponents(components, index);
+        showComponents(components);
+      });
+    });
+
+    // Clear completed tasks
+    const clearBtn = document.querySelector('.clear');
+    clearBtn.addEventListener('click', () => {
+       components = clearCompletedTasks(components);
+        showComponents(components);
     });
   });
-
-  // interactive section
-  const updateComponents = (components, index) => {
-    components[index].completed = !components[index].completed;
-    localStorage.setItem('toDoList', JSON.stringify(components));
-  };
-
-  // Checkbox status
-  const checkBoxes = document.querySelectorAll('.check-box');
-  checkBoxes.forEach((checkBox, index) => {
-    checkBox.addEventListener('change', () => {
-      updateComponents(components, index);
-      showComponents(components);
-    });
-  });
-
-  const clearCompletedTasks = () => {
-    components = components.filter((item) => item.completed === false);
-    let i = 1;
-    components.forEach((component) => {
-      component.index = i;
-      i += 1;
-    });
-    localStorage.setItem('toDoList', JSON.stringify(components));
-    showComponents(components);
-  };
-
-  // Clear completed tasks
-  const clearBtn = document.querySelector('.clear');
-  clearBtn.addEventListener('click', () => {
-    clearCompletedTasks();
-  });
-});
 
 window.addEventListener('load', () => showComponents(components));
