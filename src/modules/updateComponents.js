858199eaf21d2151097d3116ec41e71b7d9c9bd8
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-15 20:06:28 +0300

    Update updateComponents.js

--- src/modules/updateComponents.js
+++ src/modules/updateComponents.js
@@ -1,4 +1,4 @@
 export default (components, index) => {
-    components[index].completed = !components[index].completed;
-    localStorage.setItem('toDoList', JSON.stringify(components));
-}
+  components[index].completed = !components[index].completed;
+  localStorage.setItem('toDoList', JSON.stringify(components));
+};
