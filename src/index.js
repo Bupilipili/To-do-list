693da1c3239cf642afa880db5699adf43c9c79ff
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-15 16:01:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,50 +3,50 @@ import './style.css';
 import { addNewComponent, removeComponents } from './modules/functions.js';
 
 const showComponents = (components) => {
-    const container = document.querySelector('.to-do-list');
-    container.innerHTML = ''; // Clears the container
+  const container = document.querySelector('.to-do-list');
+  container.innerHTML = ''; // Clears the container
 
-    // Sort components array based on index property
-    components.sort((a, b) => a.index - b.index);
+  // Sort components array based on index property
+  components.sort((a, b) => a.index - b.index);
 
-    components.forEach((component) => {
-      const items = document.createElement('li');
-      items.className = 'list-item';
-      items.dataset.index = component.index - 1;
-      items.innerHTML = `
-      <div class="listboxflex">
-          <input class="check-box" type="checkbox" ${component.completed ? 'checked' : ''}>
-          <input class="description" value="${component.description}" readonly>
-          <a><i class="fa-solid fa-trash delete-icon"></i></a>
-      </div>
-      `;
-      container.appendChild(items);
-    });
-  };
+  components.forEach((component) => {
+    const items = document.createElement('li');
+    items.className = 'list-item';
+    items.dataset.index = component.index - 1;
+    items.innerHTML = `
+    <div class="listboxflex">
+        <input class="check-box" type="checkbox" ${component.completed ? 'checked' : ''}>
+        <input class="description" value="${component.description}" readonly>
+        <a><i class="fa-solid fa-trash delete-icon"></i></a>
+    </div>
+    `;
+    container.appendChild(items);
+  });
+};
 
-  let components = JSON.parse(localStorage.getItem('toDoList')) || [];
-  const addList = document.querySelector('.input');
-  addList.addEventListener('keyup', (event) => {
-    event.preventDefault();
-    if (event.key === 'Enter' && addList.value !== '') {
-      const componentDesc = addList.value;
-      components.push(addNewComponent(componentDesc, components.length));
+let components = JSON.parse(localStorage.getItem('toDoList')) || [];
+const addList = document.querySelector('.input');
+addList.addEventListener('keyup', (event) => {
+  event.preventDefault();
+  if (event.key === 'Enter' && addList.value !== '') {
+    const componentDesc = addList.value;
+    components.push(addNewComponent(componentDesc, components.length));
+    showComponents(components);
+    localStorage.setItem('toDoList', JSON.stringify(components));
+    addList.value = '';
+  }
+});
+
+const container = document.querySelector('.to-do-list');
+container.addEventListener('click', (event) => {
+  const remove = document.querySelectorAll('.delete-icon');
+  remove.forEach((icon, index) => {
+    if (event.target === icon) {
+      removeComponents(components, index);
       showComponents(components);
       localStorage.setItem('toDoList', JSON.stringify(components));
-      addList.value = '';
     }
   });
-
-  const container = document.querySelector('.to-do-list');
-  container.addEventListener('click', (event) => {
-    const remove = document.querySelectorAll('.delete-icon');
-    remove.forEach((icon, index) => {
-      if (event.target === icon) {
-        removeComponents(components, index);
-        showComponents(components);
-        localStorage.setItem('toDoList', JSON.stringify(components));
-      }
-    });
 
   // Edit case
   const tasksInput = document.querySelectorAll('.description');
@@ -67,7 +67,7 @@ const showComponents = (components) => {
     });
   });
 
-  //interactive section
+  // interactive section
   const updateComponents = (components, index) => {
     components[index].completed = !components[index].completed;
     localStorage.setItem('toDoList', JSON.stringify(components));
