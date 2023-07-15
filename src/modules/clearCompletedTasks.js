8870661eecee68286c5aad29038985be62bf91cd
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-15 20:06:35 +0300

    Update clearCompletedTasks.js

--- src/modules/clearCompletedTasks.js
+++ src/modules/clearCompletedTasks.js
@@ -1,10 +1,10 @@
 export default (components) => {
-    components = components.filter((item) => item.completed === false);
-    let i = 1;
-    components.forEach((component) => {
-      component.index = i;
-      i += 1;
-    });
-    localStorage.setItem('toDoList', JSON.stringify(components));
-    return components;
-}
+  components = components.filter((item) => item.completed === false);
+  let i = 1;
+  components.forEach((component) => {
+    component.index = i;
+    i += 1;
+  });
+  localStorage.setItem('toDoList', JSON.stringify(components));
+  return components;
+};
