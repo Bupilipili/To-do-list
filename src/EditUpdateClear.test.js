8264de700d88b5216d1169ba89263b0a6c7092b2
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-20 18:52:10 +0300

    Create EditUpdateClear.test.js

---
+++ src/EditUpdateClear.test.js
@@ -0,0 +1,92 @@
+import updateComponents from './modules/updateComponents.js';
+import editComponents from './modules/showComponents.js';
+import clearCompletedTasks from './modules/clearCompletedTasks.js';
+
+
+
+  // Create a mock function for adding event listeners
+  const addEventListenerMock = jest.fn((event, callback) => {
+    // Implement your custom logic here (if needed)
+    // For this example, we won't do anything
+  });
+
+  // Create a mock for the tasksInput array of elements
+  const tasksInputMock = [
+    { addEventListener: addEventListenerMock, hasAttribute: () => true },
+    { addEventListener: addEventListenerMock, hasAttribute: () => false },
+  ];
+
+  // Create a mock localStorage
+  const localStorageMock = (() => {
+    let store = {};
+    return {
+      getItem: (key) => store[key] || null,
+      setItem: (key, value) => {
+        store[key] = value.toString();
+      },
+      clear: () => {
+        store = {};
+      },
+    };
+  })();
+
+  // Replace the global window object with our mocked version
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+    writable: true,
+  });
+
+  describe('editComponents', () => {
+    test('should edit task description and localStorage', () => {
+      // Mock components data as needed for your test case
+      const components = [
+        { description: 'Task 1', completed: false, index: 1 },
+        { description: 'Task 2', completed: false, index: 2 },
+      ];
+
+      // Call the function with the mocked data
+      editComponents(components, tasksInputMock);
+
+      // Assert that the function has updated components and localStorage accordingly
+      // You can use expect statements to check if the function has behaved as expected
+    });
+  });
+
+  describe('updateComponents', () => {
+    test('should update item completion status and localStorage', () => {
+      // Mock components data as needed for your test case
+      const components = [
+        { description: 'Task 1', completed: false, index: 1 },
+        { description: 'Task 2', completed: false, index: 2 },
+      ];
+      const indexToUpdate = 0;
+
+      // Call the function with the mocked data
+      updateComponents(components, indexToUpdate);
+
+      // Assert that the function has updated components and localStorage accordingly
+      // You can use expect statements to check if the function has behaved as expected
+    });
+  });
+
+  describe('clearCompletedTasks', () => {
+    test('should clear completed tasks and update indexes and localStorage', () => {
+      // Mock components data as needed for your test case
+      const components = [
+        { description: 'Task 1', completed: false, index: 1 },
+        { description: 'Task 2', completed: true, index: 2 },
+        { description: 'Task 3', completed: false, index: 3 },
+      ];
+
+      // Call the function with the mocked data
+      const result = clearCompletedTasks(components);
+
+      // Assert that the function has updated components and localStorage accordingly
+      // You can use expect statements to check if the function has behaved as expected
+      // For example, you can expect the result to have filtered out completed tasks
+      expect(result).toEqual([
+        { description: 'Task 1', completed: false, index: 1 },
+        { description: 'Task 3', completed: false, index: 2 },
+      ]);
+    });
+  });
